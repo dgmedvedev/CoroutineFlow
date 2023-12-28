94775550e4dc6be5520c10a6a6c38e134ca459dc
dgmedvedev <dg-medvedev@mail.ru>
2023-12-28 17:31:42 +0300

    lesson9 - Hot Flow

--- app/src/main/java/com/demo/coroutineflow/lesson9.kt
+++ app/src/main/java/com/demo/coroutineflow/lesson9.kt
@@ -3,33 +3,33 @@ package com.demo.coroutineflow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 
 private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
 suspend fun main() {
-    val flow = getFlow()
+    val flow = MutableSharedFlow<Int>()
+
+    coroutineScope.launch {
+        repeat(5) {
+            println("Emitted: $it")
+            flow.emit(it)
+            delay(1000)
+        }
+    }
 
     val job1 = coroutineScope.launch {
-        println(flow.first())
+        flow.collect {
+            println("Got from 1st collector: $it")
+        }
     }
     delay(3000)
     val job2 = coroutineScope.launch {
         flow.collect {
-            println(it)
+            println("Got from 2nd collector: $it")
         }
     }
     job1.join()
     job2.join()
-}
-
-fun getFlow(): Flow<Int> = flow {
-    repeat(3) {
-        println("Emitted: $it")
-        emit(it)
-        delay(1000)
-    }
 }
